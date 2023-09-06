f436867a8848718a468747a489bc1fd9460516c1
larabr <larabr+github@protonmail.com>
2023-09-06 16:39:23 +0200

    Support generating subkeys with 'forwarded communication' flag to decrypt autoforwarded messages (#8)
    These subkeys must not have the standard encryption flags (EtEr) set,
    as they are not supposed to be used for direct messages.
    
    Also:
    - preserve 'forwarded communication' key flag when reformatting
    - fix bug allowing to decrypt forwarded messages by setting
    `config.allowInsecureDecryptionWithSigningKeys` instead of
    `config.allowForwardedMessages`
    - add TS definition for `config.allowForwardedMessages`

--- openpgp.d.ts
+++ openpgp.d.ts
@@ -326,6 +326,7 @@ interface Config {
   aeadProtect: boolean;
   allowUnauthenticatedMessages: boolean;
   allowUnauthenticatedStream: boolean;
+  allowForwardedMessages: boolean;
   minRSABits: number;
   passwordCollisionCheck: boolean;
   revocationsExpire: boolean;
@@ -711,6 +712,7 @@ export interface SubkeyOptions {
   keyExpirationTime?: number;
   date?: Date;
   sign?: boolean;
+  forwarding?: boolean;
   config?: PartialConfig;
 }
 
