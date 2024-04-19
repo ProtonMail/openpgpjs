0f45d53e0ef548893a39ec042dfab1c48edee4c6
larabr <larabr+github@protonmail.com>
2024-04-19 16:10:03 +0200

    Temporarily add `config.ignoreSEIPDv2FeatureFlag` for compatibility (#15)
    SEIPDv2 is a more secure and faster choice, but it is
    not necessarily compatible with other libs and our mobile apps.
    
    Co-authored-by: Daniel Huigens <d.huigens@protonmail.com>

--- openpgp.d.ts
+++ openpgp.d.ts
@@ -324,6 +324,7 @@ interface Config {
   showVersion: boolean;
   showComment: boolean;
   aeadProtect: boolean;
+  ignoreSEIPDv2FeatureFlag: boolean;
   allowUnauthenticatedMessages: boolean;
   allowUnauthenticatedStream: boolean;
   allowForwardedMessages: boolean;
